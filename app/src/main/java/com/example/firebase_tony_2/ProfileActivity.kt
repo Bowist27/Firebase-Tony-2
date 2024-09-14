622a4687849f1cf75539bcca9f6174a8a962972c
Tony <111481792+Bowist27@users.noreply.github.com>
2024-09-13 21:04:20 -0600

    Profile-Activity

--- app/src/main/java/com/example/firebase_tony_2/ProfileActivity.kt
+++ app/src/main/java/com/example/firebase_tony_2/ProfileActivity.kt
@@ -1,20 +1,69 @@
 package com.example.firebase_tony_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.firebase_tony_2.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding: ActivityProfileBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
+        setContentView(binding.root)
+
+        //Configure ActionBar
+        actionBar = supportActionBar ?: return
+        actionBar.title = "Profile"
+
+        //init firebase auth =
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, logout
+        binding.logoutBtn.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun checkUser() {
+        //check if user is logged or not
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            //user not null, user is logged in, get user info
+            val email = firebaseUser.email
+            //set to text view
+            binding.emailTv.text = email
+        }
+        else{
+            //User is null, user is not logged in
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
     }
 }
