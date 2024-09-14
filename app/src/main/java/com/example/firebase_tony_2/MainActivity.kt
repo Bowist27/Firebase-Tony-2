9fb3ce903595ca2eaa0ee2985135388dc64fff7b
Tony <111481792+Bowist27@users.noreply.github.com>
2024-09-13 18:20:02 -0600

    Diseño-Vistas-Pre-conexionFirebase

--- app/src/main/java/com/example/firebase_tony_2/MainActivity.kt
+++ app/src/main/java/com/example/firebase_tony_2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firebase_tony_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,20 +13,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firebase_tony_2.ui.theme.FirebaseTony2Theme
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            FirebaseTony2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        Timer().schedule(1000) {
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
+            finish()
         }
     }
 }
